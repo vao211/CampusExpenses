61050d1f78ea351c78a0337c6017b00be7058ebd
vao211 <vaolu211@gmail.com>
2025-07-18 10:29:56 +0700

    Remove : password key

--- app/src/main/java/com/example/vcampusexpenses/session/SessionManager.java
+++ app/src/main/java/com/example/vcampusexpenses/session/SessionManager.java
@@ -6,37 +6,26 @@ public class SessionManager {
     private static final String PREF_NAME = "SessionPrefs";
     private static final String KEY_EMAIL = "email";
     private static final String KEY_IS_LOGGED_IN = "isLoggedIn";
-
-    private static final String KEY_PASSWORD = "password";
-
     private final SharedPreferences pref;
     private final SharedPreferences.Editor editor;
-
     public SessionManager(Context context) {
         pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         editor = pref.edit();
     }
-
-    public void saveLoginSession(String email, String password) {
+    public void saveLoginSession(String email) {
         editor.putString(KEY_EMAIL, email);
-        editor.putString(KEY_PASSWORD, password);
         editor.putBoolean(KEY_IS_LOGGED_IN, true);
         editor.apply();
     }
     public boolean isLoggedIn() {
         return pref.getBoolean(KEY_IS_LOGGED_IN, false);
     }
-
     public String getSavedEmail() {
         return pref.getString(KEY_EMAIL, null);
     }
-
-    public String getSavedPassword() {
-        return pref.getString(KEY_PASSWORD, null);
-    }
-
     public void logout() {
         editor.clear();
         editor.apply();
+
     }
 }
