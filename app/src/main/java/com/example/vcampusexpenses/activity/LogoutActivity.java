924b5d3bbf9704ce255ec2f5f4f920a8ef1fbb78
vao211 <vaolu211@gmail.com>
2025-07-26 20:58:48 +0700

    Add: Activity Logut

---
+++ app/src/main/java/com/example/vcampusexpenses/activity/LogoutActivity.java
@@ -0,0 +1,29 @@
+package com.example.vcampusexpenses.activity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.vcampusexpenses.R;
+
+public class LogoutActivity extends AppCompatActivity {
+    private ImageView gifLoading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+        gifLoading = findViewById(R.id.gif_loading);
+        loadGif();
+
+    }
+
+    private void loadGif() {
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.loading)
+                .into(gifLoading);
+    }
+}
