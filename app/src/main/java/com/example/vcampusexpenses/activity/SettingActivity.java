bfe67f8408666e8ef0911b48fe34f4cf2d8a1e49
vao211 <Vaolu211@gmail.com>
2025-07-28 21:05:18 +0700

    Feature: Setting

--- app/src/main/java/com/example/vcampusexpenses/activity/SettingActivity.java
+++ app/src/main/java/com/example/vcampusexpenses/activity/SettingActivity.java
@@ -1,26 +1,64 @@
 package com.example.vcampusexpenses.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vcampusexpenses.R;
 import com.example.vcampusexpenses.authentication.FireBaseAuthen;
 import com.example.vcampusexpenses.authentication.GuestAuthen;
+import com.example.vcampusexpenses.services.SettingService;
 import com.example.vcampusexpenses.session.SessionManager;
 
+import java.util.Objects;
+
 public class SettingActivity extends AppCompatActivity {
-    Button btn_logout;
+    Button btn_login;
+    ImageButton btn_logout;
+    TextView txt_displayName, txt_type_currency, txt_description_notif, txt_description_sync, txt_email;
     SessionManager sessionManager;
+    SettingService settingService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         sessionManager = new SessionManager(this);
         btn_logout = findViewById(R.id.btn_logout);
+        btn_login = findViewById(R.id.btn_login);
+        txt_displayName = findViewById(R.id.txt_displayName);
+        txt_type_currency = findViewById(R.id.txt_type_currency);
+        txt_description_notif = findViewById(R.id.txt_description_notif);
+        txt_description_sync = findViewById(R.id.txt_description_sync);
+        txt_email = findViewById(R.id.txt_email);
+        settingService = new SettingService(this);
 
+        loadLoginState();
+        loadSetting();
         LogOut();
+    }
+    private void loadLoginState(){
+        String userId = sessionManager.getUserId();
+        if(!userId.equals("Guest")){
+            btn_login.setVisibility(View.GONE);
+            txt_email.setVisibility(View.VISIBLE);
+            txt_email.setText(sessionManager.getSavedEmail());
+        }
+    }
+    private void loadSetting(){
+        String displayName = settingService.getDisplayName();
+        String currency = settingService.getCurrency();
+        boolean notification = settingService.getNotification();
+        txt_displayName.setText(displayName);
+        txt_type_currency.setText(currency);
+        if(notification){
+            txt_description_notif.setText("On");
+        }else{
+            txt_description_notif .setText("Off");
+        }
     }
 
     private void LogOut(){
