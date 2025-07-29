2e22a69a6a590c52f376432afabcc4fece211173
vao211 <Vaolu211@gmail.com>
2025-07-29 19:08:38 +0700

    Add: createTransaction view, border.xml

---
+++ app/src/main/java/com/example/vcampusexpenses/activity/AddTransactionActivity.java
@@ -0,0 +1,15 @@
+package com.example.vcampusexpenses.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vcampusexpenses.R;
+
+public class AddTransactionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtransaction);
+    }
+}
--- app/src/main/java/com/example/vcampusexpenses/fragments/BudgetFragment.java
+++ app/src/main/java/com/example/vcampusexpenses/fragments/BudgetFragment.java
@@ -12,6 +12,6 @@ import com.example.vcampusexpenses.R;
 public class BudgetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_analysis, container, false);
+        return inflater.inflate(R.layout.fragment_budget, container, false);
     }
 }
