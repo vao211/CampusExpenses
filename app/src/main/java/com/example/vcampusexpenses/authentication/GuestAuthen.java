b59672625fa53ec02c2367ff9fedb8763dd40c08
vao211 <Vaolu211@gmail.com>
2025-07-29 14:03:27 +0700

    Fix: guest display name in setting

--- app/src/main/java/com/example/vcampusexpenses/authentication/GuestAuthen.java
+++ app/src/main/java/com/example/vcampusexpenses/authentication/GuestAuthen.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 
 import com.example.vcampusexpenses.activity.LoginActivity;
 import com.example.vcampusexpenses.activity.MainActivity;
+import com.example.vcampusexpenses.datamanager.SettingDataManager;
 import com.example.vcampusexpenses.datamanager.UserDataManager;
+import com.example.vcampusexpenses.model.Setting;
+import com.example.vcampusexpenses.services.SettingService;
 import com.example.vcampusexpenses.session.SessionManager;
 import com.example.vcampusexpenses.utils.DisplayToast;
 
@@ -16,6 +19,8 @@ public class GuestAuthen {
     public static void LogIn(Context context){
         SessionManager sessionManager = new SessionManager(context);
         sessionManager.saveLoginSession("Guest", "Guest");
+        SettingService settingService = new SettingService(context);
+        settingService.setDisplayName("Guest");
         createSampleData(context);
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
