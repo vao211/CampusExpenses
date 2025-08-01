e0c58cdf6d3d5cb3acee8d67cecfbb1deb513436
vao211 <vaolu211@gmail.com>
2025-08-01 13:40:05 +0700

    Feat: View, Delete, Edit transaction

--- app/src/main/java/com/example/vcampusexpenses/fragments/TransactionFragment.java
+++ app/src/main/java/com/example/vcampusexpenses/fragments/TransactionFragment.java
@@ -8,13 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vcampusexpenses.R;
-import com.example.vcampusexpenses.activity.AddTransactionActivity;
+import com.example.vcampusexpenses.activity.TransactionActivity;
+import com.example.vcampusexpenses.adapters.TransactionAdapter;
 import com.example.vcampusexpenses.datamanager.UserDataManager;
+import com.example.vcampusexpenses.model.Transaction;
 import com.example.vcampusexpenses.services.AccountService;
 import com.example.vcampusexpenses.services.BudgetService;
 import com.example.vcampusexpenses.services.CategoryService;
@@ -24,11 +29,14 @@ import com.example.vcampusexpenses.utils.DateFilterUtil;
 import com.example.vcampusexpenses.utils.DateFilterView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
-public class TransactionFragment extends Fragment {
-    String TAG = "TransactionFragment";
+public class TransactionFragment extends Fragment implements TransactionAdapter.OnTransactionListener {
+    private static final String TAG = "TransactionFragment";
     private DateFilterView dateFilterView;
     private DateFilterUtil dateFilterUtil;
     private LinearLayout llIncome, llOutcome, llTransfer, llAll;
@@ -38,8 +46,11 @@ public class TransactionFragment extends Fragment {
     private CategoryService categoryService;
     private BudgetService budgetService;
     private TransactionService transactionService;
-    private String transactionType;
-    ImageButton btnAdd;
+    private String transactionType = "all";
+    private RecyclerView rvTransactions;
+    private TransactionAdapter transactionAdapter;
+    private ImageButton btnAdd;
+    private TextView txtNoTransactions;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -54,33 +65,60 @@ public class TransactionFragment extends Fragment {
         transactionService = new TransactionService(dataManager, accountService, budgetService);
 
         btnAdd = view.findViewById(R.id.btn_add);
-
         llIncome = view.findViewById(R.id.ll_income);
         llOutcome = view.findViewById(R.id.ll_outcome);
         llTransfer = view.findViewById(R.id.ll_transfer);
         llAll = view.findViewById(R.id.ll_all);
+        txtNoTransactions = view.findViewById(R.id.txt_no_transactions);
 
+        //recyclerView
+        rvTransactions = view.findViewById(R.id.rv_transactions);
+        rvTransactions.setLayoutManager(new LinearLayoutManager(requireContext()));
+        transactionAdapter = new TransactionAdapter(dataManager,transactionService, accountService, categoryService, this);
+        rvTransactions.setAdapter(transactionAdapter);
 
         dateFilterView = view.findViewById(R.id.date_filter_view);
         initDateFilter(savedInstanceState);
         setTransactionTypeSelection();
         setTransactionTypeSelection("all");
         addTransaction();
+        loadTransactions(null, null, DateFilterUtil.FilterType.THIS_MONTH);
         return view;
     }
-    private void addTransaction(){
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadTransactions(dateFilterUtil.getStartDate(), dateFilterUtil.getEndDate(), dateFilterUtil.getCurrentFilterType());
+    }
+
+    private void addTransaction() {
         btnAdd.setOnClickListener(v -> {
-            Intent intent = new Intent(requireContext(), AddTransactionActivity.class);
+            Intent intent = new Intent(requireContext(), TransactionActivity.class);
             startActivity(intent);
         });
     }
+
     private void setTransactionTypeSelection() {
         setTransactionTypeSelection(null);
-        llIncome.setOnClickListener(v -> setTransactionTypeSelection("income"));
-        llOutcome.setOnClickListener(v -> setTransactionTypeSelection("outcome"));
-        llTransfer.setOnClickListener(v -> setTransactionTypeSelection("transfer"));
-        llAll.setOnClickListener(v -> setTransactionTypeSelection("all"));
+        llIncome.setOnClickListener(v -> {
+            setTransactionTypeSelection("income");
+            loadTransactions(dateFilterUtil.getStartDate(), dateFilterUtil.getEndDate(), dateFilterUtil.getCurrentFilterType());
+        });
+        llOutcome.setOnClickListener(v -> {
+            setTransactionTypeSelection("outcome");
+            loadTransactions(dateFilterUtil.getStartDate(), dateFilterUtil.getEndDate(), dateFilterUtil.getCurrentFilterType());
+        });
+        llTransfer.setOnClickListener(v -> {
+            setTransactionTypeSelection("transfer");
+            loadTransactions(dateFilterUtil.getStartDate(), dateFilterUtil.getEndDate(), dateFilterUtil.getCurrentFilterType());
+        });
+        llAll.setOnClickListener(v -> {
+            setTransactionTypeSelection("all");
+            loadTransactions(dateFilterUtil.getStartDate(), dateFilterUtil.getEndDate(), dateFilterUtil.getCurrentFilterType());
+        });
     }
+
     private void setTransactionTypeSelection(String selected) {
         llIncome.setBackgroundResource(R.drawable.border);
         llOutcome.setBackgroundResource(R.drawable.border);
@@ -102,31 +140,73 @@ public class TransactionFragment extends Fragment {
         }
     }
 
-    //date filter
+    private void loadTransactions(Calendar startDate, Calendar endDate, DateFilterUtil.FilterType filterType) {
+        List<Transaction> transactions = transactionService.getListTransactions();
+        List<Transaction> filteredTransactions = new ArrayList<>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+
+        for (Transaction transaction : transactions) {
+            try {
+                if (!"all".equalsIgnoreCase(transactionType) && !transaction.getType().equalsIgnoreCase(transactionType)) {
+                    continue;
+                }
+
+                if (startDate != null && endDate != null) {
+                    String transactionDate = transaction.getDate();
+                    Calendar transDate = Calendar.getInstance();
+                    transDate.setTime(dateFormat.parse(transactionDate));
+                    setCalendarToBeginningOfDay(transDate);
+                    if (transDate.before(startDate) || transDate.after(endDate)) {
+                        continue;
+                    }
+                }
+
+                filteredTransactions.add(transaction);
+            } catch (Exception e) {
+                Log.e(TAG, "Error parse transaction date: " + e.getMessage(), e);
+            }
+        }
+
+            Collections.reverse(filteredTransactions);
+//        Collections.sort(filteredTransactions, (t1, t2) -> t2.getDate().compareTo(t1.getDate()));
+        transactionAdapter.setTransactions(filteredTransactions);
+        if (filteredTransactions.isEmpty()) {
+            txtNoTransactions.setVisibility(View.VISIBLE);
+            rvTransactions.setVisibility(View.GONE);
+        } else {
+            txtNoTransactions.setVisibility(View.GONE);
+            rvTransactions.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setCalendarToBeginningOfDay(Calendar cal) {
+        if (cal == null) return;
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+    }
+
+    private void initDateFilter(Bundle savedInstanceState) {
+        dateFilterUtil = new DateFilterUtil(requireContext(), getParentFragmentManager());
+        dateFilterView.setDateFilterUtil(dateFilterUtil, getParentFragmentManager());
+        dateFilterUtil.setOnDateFilterChangedListener(this::loadTransactions);
+
+        if (savedInstanceState != null) {
+            dateFilterUtil.restoreInstanceState(savedInstanceState);
+        }
+    }
+
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         dateFilterUtil.saveInstanceState(outState);
     }
 
-    private void loadDataForCurrentPeriod(Calendar startDate, Calendar endDate, DateFilterUtil.FilterType filterType) {
-        if (startDate == null || endDate == null) {
-            Log.e(TAG, "Cannot load data: startDate or endDate is null.");
-            return;
-        }
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        Log.i(TAG, "Loading data for period: " +
-                dateFormat.format(startDate.getTime()) + " to " +
-                dateFormat.format(endDate.getTime()));
-    }
-    private void initDateFilter(Bundle savedInstanceState) {
-        dateFilterUtil = new DateFilterUtil(requireContext(), getParentFragmentManager());
-        dateFilterView.setDateFilterUtil(dateFilterUtil, getParentFragmentManager());
-        dateFilterUtil.setOnDateFilterChangedListener((startDate, endDate, filterType) -> loadDataForCurrentPeriod(startDate, endDate, filterType));
-
-        if (savedInstanceState != null) {
-            dateFilterUtil.restoreInstanceState(savedInstanceState);
-        }
+    @Override
+    public void onEditTransaction(Transaction transaction) {
+        Intent intent = new Intent(requireContext(), TransactionActivity.class);
+        intent.putExtra("transactionId", transaction.getTransactionId());
+        startActivity(intent);
     }
 }
